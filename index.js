cf99ac0197f3bf64fe551bf53327f491b362a653
Chang Feng <chang_196700@hotmail.com>
2018-09-02 18:33:18 +0800

    basic
    Signed-off-by: Chang Feng <chang_196700@hotmail.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+var assign = require('deep-assign');
+var renderer = require('./lib/renderer');
+
+hexo.config.mermaid = assign({
+  mermaid: 'https://unpkg.com/mermaid@8.0.0-rc.8/dist/mermaid.js',
+  css: '',
+  options: {
+    startOnLoad: false
+  }
+}, hexo.config.mermaid);
+
+hexo.extend.filter.register('before_post_render', renderer.render, 9);
